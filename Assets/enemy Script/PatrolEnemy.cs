cffa9c5448e155839423909f86c953be511fbdcf
Jasswinder Singh <jasswinder3385@gmail.com>
2025-09-07 14:37:44 +0530

    UDATE IN THE ENEMY SCRIPT AND THE ANIMATION

--- Assets/enemy Script/PatrolEnemy.cs
+++ Assets/enemy Script/PatrolEnemy.cs
@@ -1,18 +1,76 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PatrolEnemy : MonoBehaviour
 {
+    public bool facingLeft = true;
+    public float moveSpeed = 2f;
+    public Transform checkPoint;
+    public float distance = 1f;
+    public LayerMask layerMask;
+    private bool inRange = false;
+    public Transform player;
+    public float attackRange = 10f;
+    public float retrieveDistance = 2.5f;
+    public float chaseSpeed = 3f;
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector2.Distance(transform.position, player.position) <= attackRange)
+        {
+            inRange = true;
+        }
+        else
+        {
+            inRange = false;
+        }
+        if (inRange)
+        {
+            if (Vector2.Distance(transform.position, player.position) >= retrieveDistance)
+            {
+                animator.SetBool("Attack1", false);
+                transform.position = Vector2.MoveTowards(transform.position, player.position, chaseSpeed * Time.deltaTime);
+            }
+            else
+            {
+                animator.SetBool("Attack1", true);
+            }
+           
+        }
+        else
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * moveSpeed);
+
+            Physics2D.Raycast(checkPoint.position, Vector2.down, distance, layerMask);
+            RaycastHit2D hit = Physics2D.Raycast(checkPoint.position, Vector2.down, distance, layerMask);
+            if (hit == false && facingLeft)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                facingLeft = false;
+            }
+            else if (hit == false && facingLeft == false)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                facingLeft = true;
+
+            }
+        }
+
+
+    }
+    private void OnDrawGizmos()
+    {
+        if (checkPoint == null)
+            return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(checkPoint.position, new Vector3(checkPoint.position.x, checkPoint.position.y - distance, checkPoint.position.z));
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
     }
 }
