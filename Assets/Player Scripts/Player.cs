722bb02426a0fd36b583636a54b090b201ee534e
Rama Mangtani <mangtanirama14@gmail.com>
2025-09-13 20:17:02 +0530

    update the code with new Functionality

--- Assets/Player Scripts/Player.cs
+++ Assets/Player Scripts/Player.cs
@@ -17,7 +17,7 @@ public class NewBehaviourScript : MonoBehaviour
 
     }
 
-    // Update is called once per frame
+    //Update is called once per frame.
     void Update()
     {
         movement = Input.GetAxis("Horizontal");
